cfbfe4bba6e1bbde5aaabe68c84c7cca813e50bc
Simonmohapatra <146565082+Simonmohapatra@users.noreply.github.com>
2024-05-28 10:27:46 +0530

    Q3.java

--- Q3.java
+++ Q3.java
@@ -1,1 +1,38 @@
+/*
+Create a class Student & enter mark, name of the student. 
+If mark is more than 100, create exception MarksOutOfBoundException & throw it using Java.
+*/
 
+class MarksOutOfBoundException extends Exception{
+    public String MarksOutOfBoundException(String message){
+       return message;
+    }
+}
+class Student{
+    String name;
+    int mark;
+    public void display(){
+        System.out.println("Name: "+name);
+        System.out.println("Marks: "+mark);
+    }
+}
+
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        Student s1= new Student();
+        System.out.println("Enter the name of the student1: ");
+        s1.name=sc.nextLine();
+        System.out.println("Enter the marks of the student1: ");
+        try {
+            s1.mark=sc.nextInt();
+            if (s1.mark>100){
+                throw new MarksOutOfBoundException();
+            }
+            s1.display();
+        }
+        catch (MarksOutOfBoundException e){
+            System.out.println(e+" Marks can't be greater than 100");
+        }
+    }
+}
