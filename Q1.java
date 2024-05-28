b8c6123d7b003cd8207f8fd0dff30a58c073602b
Simonmohapatra <146565082+Simonmohapatra@users.noreply.github.com>
2024-05-28 09:26:19 +0530

    Q1.java

--- Q1.java
+++ Q1.java
@@ -1,1 +1,23 @@
+/*
+Write a Java program to read your lucky number from keyboard. 
+Treat –ve no. as NumberFormatException. 
+Write appropriate Exceptional handler.
+*/
 
+import java.util.Scanner;
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter your lucky number: ");
+        int a=sc.nextInt();
+        try{
+            if (a<0){
+                throw new NumberFormatException("Negative number");
+            }
+            System.out.println("Your lucky number is "+a);
+        }
+        catch (NumberFormatException e){
+            System.out.println(e);
+        }
+    }
+}
